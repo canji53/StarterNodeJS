21d9a50602f9af2e49d5556d09ef11eba5b1af85
canji53 <canji.preengineer@gmail.com>
2020-11-08 20:47:01 +0900

    :tada: B082

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,36 @@
-import { default as hello } from '../src/index'
+import { default as main, permutation } from '../src/index'
 
 describe('Jest ESModule Mock', () => {
-  test('...', () => {
-    expect(hello()).toEqual('Hello Jest!!')
+  test('case1', () => {
+    const inputString = `10 3
+work
+off
+off
+work
+work
+work
+off
+work
+work
+off`
+    expect(main(inputString)).toEqual(6)
+  })
+
+  test('case2', () => {
+    const inputString = `8 3
+work
+off
+off
+work
+off
+work
+off
+work`
+    expect(main(inputString)).toEqual(7)
+  })
+
+  test('permutation', () => {
+    const result = permutation([], [], [0, 1, 2, 3, 4, 5], 3)
+    expect(result.length).toEqual(120)
   })
 })
--- src/index.js
+++ src/index.js
@@ -1,10 +1,73 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-console.log(process.env.SAMPLE)
+const main = (inputString) => {
+  const data = JSON.parse(JSON.stringify(inputString)).split('\n')
+  const [_, nPaidVacation] = data
+    .shift()
+    .split(' ')
+    .map((_) => parseInt(_))
 
-const hello = () => {
-  return 'Hello Jest!!'
+  const schedules = data // スケジュール
+
+  // 仕事日を休日に変えるパターンをインデックスの配列で取得
+  const workdayIndexes = schedules
+    .map((schedule, i) => (schedule === 'work' ? i : false))
+    .filter((number) => Number.isInteger(number))
+  const patterns = permutation([], [], workdayIndexes, nPaidVacation)
+
+  // 休日パターンをスケジュールに反映
+  const holidayScheduleMap = patterns.map((pattern) => {
+    const copySchedules = JSON.parse(JSON.stringify(schedules))
+    pattern.map((i) => (copySchedules[i] = 'off'))
+    return copySchedules
+  })
+
+  // 休日連鎖を各パターンで検査
+  const nConsecutiveHolidayMap = holidayScheduleMap.map((schedules) => {
+    const consecutiveHolidays = []
+    let countConsecutive = 0
+    schedules.map((schedule, i) => {
+      // 初日が off の場合
+      if (i === 0 && schedule === 'off') {
+        countConsecutive += 1
+        return
+      }
+      // work の場合、off をストップして配列に追加
+      if (schedule === 'work') {
+        consecutiveHolidays.push(countConsecutive)
+        countConsecutive = 0
+        return
+      }
+      // 最終日が off の場合、off を加算して必ず配列に追加
+      if (i === schedules.length - 1 && schedule === 'off') {
+        countConsecutive += 1
+        consecutiveHolidays.push(countConsecutive)
+        countConsecutive = 0
+      }
+      // 連休の場合は、連続カウンターを加算
+      countConsecutive += 1
+    })
+    return Math.max(...consecutiveHolidays)
+  })
+
+  const nMaxConsecutiveHoliday = Math.max(...nConsecutiveHolidayMap)
+
+  return nMaxConsecutiveHoliday
 }
 
-export default hello
+export const permutation = (perm, pre, post, n) => {
+  // 再帰で順列計算
+  if (n > 0) {
+    ;[...Array(post.length).keys()].map((i) => {
+      const rest = JSON.parse(JSON.stringify(post)) // 配列の複製
+      const elem = rest.splice(i, 1)
+      permutation(perm, pre.concat(elem), rest, n - 1)
+    })
+  } else {
+    perm.push(pre)
+  }
+  return perm
+}
+
+export default main
