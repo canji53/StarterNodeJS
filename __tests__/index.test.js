46862759329f475438b808a82a14136a6344e30c
canji53 <canji.preengineer@gmail.com>
2020-11-10 00:15:48 +0900

    :tada: B025

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,18 @@
 import { default as main } from '../src/index'
 
 describe('Jest ESModule Mock', () => {
-  test('...', () => {
-    expect(main()).toEqual('Hello Jest!!')
+  test('case1', () => {
+    const inputString = `5 2 2
+1
+2`
+    expect(main(inputString)).toEqual([5, 1])
+  })
+
+  test('case2', () => {
+    const inputString = `6 3 6
+1
+3
+5`
+    expect(main(inputString)).toEqual([1, 3, 5])
   })
 })
--- src/index.js
+++ src/index.js
@@ -1,8 +1,51 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-const main = () => {
-  return 'Hello Jest!!'
+const main = (inputString) => {
+  const data = JSON.parse(JSON.stringify(inputString)).split('\n')
+  const [nBushes, nRabbit, nStep] = data
+    .shift()
+    .split(' ')
+    .map((_) => parseInt(_))
+
+  // ウサギの現在のインデックス値
+  const rabbitsIndexes = data.map((_) => parseInt(_) - 1)
+
+  // 各ステップのループ
+  ;[...Array(nStep).keys()].map(() => {
+    // ウサギたちのジャンプ
+    ;[...Array(nRabbit).keys()].map((j) => {
+      // ウサギの現在のインデックス
+      const currentIndex = rabbitsIndexes[j]
+
+      // ジャンプ先の検証
+      let nextIndex = currentIndex
+      ;[...Array(nBushes).keys()]
+        .map((_) => _ + 1)
+        .some((i) => {
+          let possibilityIndex = currentIndex + i // 次のインデックス（ジャンプ先）
+
+          // 茂みの数を越えた場合は、はじめの茂みのインデックスに戻る
+          if (possibilityIndex > nBushes - 1) {
+            possibilityIndex = 0
+          }
+
+          // 次のインデックスがウサギたちがいるインデックスに含まれていなければ、ジャンプ成功!!
+          if (rabbitsIndexes.indexOf(possibilityIndex) === -1) {
+            nextIndex = possibilityIndex
+            return true
+          }
+        })
+
+      // ジャンプ
+      rabbitsIndexes[j] = nextIndex
+    })
+  })
+
+  const result = rabbitsIndexes.map((i) => i + 1)
+  console.log(result)
+
+  return result
 }
 
 export default main
