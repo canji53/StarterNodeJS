3b9768fceed015563afbd22e58b331099a419d3d
kanji <smiler@yamaguchikanjinoMacBook-Pro.local>
2020-11-12 16:55:19 +0900

    :tada:  Cu..Ap.

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,43 @@
-import { default as main } from '../src/index'
+import { default as findVowels } from '../src/index'
 
 describe('Jest ESModule Mock', () => {
-  test('...', () => {
-    expect(main()).toEqual('Hello Jest!!')
+  test('case1', () => {
+    const inputString = 'hello'
+    expect(findVowels(inputString)).toEqual(2)
+  })
+
+  test('case2', () => {
+    const inputString = 'why'
+    expect(findVowels(inputString)).toEqual(0)
+  })
+
+  test('case3', () => {
+    const inputString = 'HELLO'
+    expect(findVowels(inputString)).toEqual(2)
+  })
+
+  test('case4', () => {
+    const inputString = 'WHY'
+    expect(findVowels(inputString)).toEqual(0)
+  })
+
+  test('case5', () => {
+    const inputString = 'aiueo'
+    expect(findVowels(inputString)).toEqual(5)
+  })
+
+  test('case6', () => {
+    const inputString = 'AIUEO'
+    expect(findVowels(inputString)).toEqual(5)
+  })
+
+  test('case7', () => {
+    const inputString = 'abcdefghijklmnopqrstuvwxyz'
+    expect(findVowels(inputString)).toEqual(5)
+  })
+
+  test('case8', () => {
+    const inputString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    expect(findVowels(inputString)).toEqual(5)
   })
 })
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import dotenv from 'dotenv'
+
 dotenv.config()
 
-const main = () => {
-  return 'Hello Jest!!'
+const findVowels = (inputString) => {
+  const vowels = ['a', 'i', 'u', 'e', 'o', 'A', 'I', 'U', 'E', 'O']
+  const results = inputString.split('').filter(str => vowels.includes(str))
+  return results.length
 }
 
-export default main
+export const findVowelsA = (inputString) => {
+  const pattern = /[aiueo]/gi
+  const result = inputString.match(pattern)
+  return result ? result.length : 0
+}
+
+export default findVowels
